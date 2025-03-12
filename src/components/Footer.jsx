5d34c7ab789983e9326a3d197c80832029bf258e
KernelOS <romiluna04@gmail.com>
2025-03-12 19:46:29 -0300

    agrego redes al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 import { Box, Flex, Text, Link, Icon } from "@chakra-ui/react";
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
     return (
       <Box bgGradient="linear(to-r, purple.500, pink.700)" py={4} mt={4} textAlign="center">
         <Flex justify="center" gap={6} mb={2}>
-          <Link href="#" isExternal>
-            <Icon as={FaFacebook} boxSize={6} color="white" />
+          <Link href="https://github.com/Romiluu" isExternal>
+            <Icon as={FaGithub} boxSize={6} color="white" />
           </Link>
-          <Link href="#" isExternal>
+          <Link href="https://www.instagram.com/romiiluuu/" isExternal>
             <Icon as={FaInstagram} boxSize={6} color="white" />
           </Link>
-          <Link href="#" isExternal>
-            <Icon as={FaTwitter} boxSize={6} color="white" />
+          <Link href="https://www.linkedin.com/in/romina-luna04/" isExternal>
+            <Icon as={FaLinkedin} boxSize={6} color="white" />
           </Link>
         </Flex>
   
